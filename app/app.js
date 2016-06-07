5afd89a0dd31f2bbb5768426b6a5621b7dc67d8f
Ricardo <ricardo.miamihitek@gmail.com>
2016-06-07 15:12:51 -0500

    Se crean controladores iniciales

--- app/app.js
+++ app/app.js
@@ -1,1 +1,16 @@
-app.js
+var app = angular.module('AngularTemplate', [ 
+		'ngRoute'
+	]);
+
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+        
+        .when('/Index', {
+            templateUrl : 'app/main/main.html',
+            controller  : 'EventsController'       
+    	})
+    	.when('/Dashboard', {
+            templateUrl : 'app/dashboard/dashboard.html',
+            controller  : 'DashboardController'
+		});
+}]);
--- app/main/main.js
+++ app/main/main.js
@@ -1,1 +1,8 @@
-main.js
+app.controller('EventsController', ['$scope','$location',function($scope, $location) {
+	$scope.goDashboard= function(){
+		$location.path( "/Dashboard" );
+	}
+	$scope.goIndex= function(){
+		$location.path( "/Index" );
+	}
+}]);
